43f433efffc6756b625d1c3db63b4249bb4199a4
josefdra <josefdraexl@icloud.com>
2025-04-01 15:29:43 +1300

    Implemented package core_database_service and created package users_database_service

---
+++ packages/users/users_database_service/lib/src/users_database_service.dart
@@ -0,0 +1,8 @@
+/// {@template users_database_service}
+/// A flutter implementation of the user UserDatabaseService that uses 
+/// CoreDatabaseService and sqflite.
+/// {@endtemplate}
+class UsersDatabaseService {
+  /// {@macro users_database_service}
+  const UsersDatabaseService();
+}
---
+++ packages/users/users_database_service/lib/users_database_service.dart
@@ -0,0 +1,5 @@
+/// A flutter implementation of the user UserDatabaseService that uses 
+/// CoreDatabaseService and sqflite.
+library;
+
+export 'src/users_database_service.dart';
