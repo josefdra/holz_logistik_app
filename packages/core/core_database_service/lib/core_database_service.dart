e1fb19291ecabcac292b0e848bf347a100d73a1b
josefdra <josefdraexl@icloud.com>
2025-04-01 16:25:59 +1300

    finished implementing package core_database_service and user_database_service.

---
+++ packages/core/core_database_service/lib/core_database_service.dart
@@ -0,0 +1,4 @@
+/// A flutter package that handles core database operations.
+library core_database_service;
+
+export 'src/core_database_service.dart';
---
+++ packages/core/core_database_service/test/src/core_database_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import '../../lib/core_database_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CoreDatabase', () {
+    test('can be instantiated', () {
+      expect(CoreDatabase(), isNotNull);
+    });
+  });
+}
