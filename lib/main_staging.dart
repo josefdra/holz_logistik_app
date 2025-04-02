dc4b0a34575105c7ebc6cff340d4ee9e53a86720
josefdra <josefdraexl@icloud.com>
2025-04-03 08:00:20 +1300

    cleaned packaged structure

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,8 +1,8 @@
-import 'package:core_database_service/core_database_service.dart';
+import 'package:core_local_storage/core_local_storage.dart';
 import 'package:core_sync_service/core_sync_service.dart';
 import 'package:flutter/widgets.dart';
 import 'package:holz_logistik/bootstrap.dart';
-import 'package:user_database_service/user_database_service.dart';
+import 'package:user_local_storage/user_local_storage.dart';
 import 'package:user_sync_service/user_sync_service.dart';
 
 Future<void> main() async {
@@ -10,9 +10,9 @@ Future<void> main() async {
 
   const url = 'ws://localhost:8080/ws';
 
-  final coreDatabaseService = CoreDatabaseService();
+  final coreLocalStorage = CoreLocalStorage();
   final userApi =
-      UserDatabaseService(coreDatabaseService: coreDatabaseService);
+      UserLocalStorage(coreLocalStorage: coreLocalStorage);
   final coreSyncService = CoreSyncService(url: url);
   final userSyncService = UserSyncService(coreSyncService: coreSyncService);
 
